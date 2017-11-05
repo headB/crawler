48ffdd13d1c0eae15974f9658842fb86783af137
beetle from docker scrapy <775121173@qq.com>
2017-11-05 13:21:43 +0800

    modified some code

--- btou/spiders/itcast.py
+++ btou/spiders/itcast.py
@@ -3,11 +3,15 @@ from scrapy.contrib.spiders import CrawlSpider,Rule
 from btou.items import ItcastItem
 class ItcastSpider(CrawlSpider):
    name = "itcast"
-   parsexx = 'xx' 
-   def __init__(self,category=None):
+   url = 'xx' 
+   def __init__(self,url=None):
     super(ItcastSpider,self).__init__()
+    print(self.url)
     print("kumanxuan")
-    print(category)
+    self.url=url
+    print(self.url)
+    print("this is new ???")
+    print(self.url)
     #self.allowed_domains = ["itcast.cn"]
     #self.start_urls = ["http://www.itcast.cn/"]
     #rules = [Rule(LinkExtractor(allow=r"/subject/*"),'parse_item')]
@@ -15,10 +19,12 @@ class ItcastSpider(CrawlSpider):
     #print("kumanxuan")
 #	self.parsexx=category 
 
-   allowed_domains = ["itcast.cn"]
+   #allowed_domains = ["itcast.cn"]
+   
    start_urls = ["http://www.itcast.cn/"]
    rules = [Rule(LinkExtractor(allow=r"/subject/*"),'parse_item')]   
-   print(parsexx)
+   print("this is new url or not??")
+   print(url)
 
    def parse_item(self,response):
 
